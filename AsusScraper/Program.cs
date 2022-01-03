47b0d42fdb72c32fd2fa1a0192a60d26cbce4890
David Kang <3746349+hahagu@users.noreply.github.com>
2022-01-03 17:18:42 +0900

    String comparison method

--- AsusScraper/Program.cs
+++ AsusScraper/Program.cs
@@ -17,6 +17,8 @@ namespace AsusScraper
     {
         private static string moboListUrl = "https://www.asus.com/support/api/product.asmx/GetPDLevel?website=global&type=2&typeid=1156,0&productflag=1";
         private static string moboBaseUrl = "https://www.asus.com/support/api/product.asmx/GetPDBIOS?website=korea&pdhashedid=";
+        private static double query_tolerance = 0.9;
+        private static double string_tolerance = 0.6;
         static void Main()
         {
             string hash = searchMobo();
@@ -91,7 +93,7 @@ namespace AsusScraper
 
                     try
                     {
-                        Product selectedMobo = productArray.First(product => product.PDName.ToUpper().Contains(moboName.ToUpper()));
+                        Product selectedMobo = productArray.First(product => compareString(product.PDName, moboName) > query_tolerance);
                         string? selectedMoboHash = selectedMobo.PDHashedId;
 
                         if (selectedMoboHash == null)
@@ -125,10 +127,35 @@ namespace AsusScraper
                 string responseText = reader.ReadToEnd();
                 dynamic jsonArray = JObject.Parse(responseText);
                 string downloadLink = jsonArray.Result.Obj[0].Files[0].DownloadUrl.Global;
-                string filteredLink = Regex.Replace(downloadLink, "[0-9]{4}.ZIP", "");
+                string filteredLink = Regex.Replace(downloadLink, "(?i)[0-9]{4}.ZIP", "");
                 Console.WriteLine("Configured for Link: " + filteredLink + "0000.ZIP");
                 return filteredLink;
             }
+        }
+
+        static double compareString(string source, string target)
+        {
+            List<string> source_exploded = source.Replace('-', ' ').Replace('_', ' ').Split(' ').ToList();
+            List<string> target_exploded = target.Replace('-', ' ').Replace('_', ' ').Split(' ').ToList();
+
+            double score = 0.0;
+            int source_length = source_exploded.Count();
+            int target_length = target_exploded.Count();
+            int matching_words = 0;
+
+            foreach (string source_string in source_exploded)
+            {
+                foreach (string target_string in target_exploded)
+                {
+                    if (source_string.ToUpper() == target_string.ToUpper())
+                    {
+                        matching_words++;
+                    }
+                }
+            }
+
+            score = (double)matching_words / (double)target_length;
+            return score;
         }
     }
 }
